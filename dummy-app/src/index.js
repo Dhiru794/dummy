017832edbaf71743a5b5f1fd2d2e1037ad90824c
Dhiraj Yogiraj Chaudhari <77576169+Dhiru794@users.noreply.github.com>
2023-05-13 19:15:30 +0530

    My Second Commit

--- dummy-app/src/index.js
+++ dummy-app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FirstPage from './Component/FirstPage';
+import SecondHeader from './Component/SecondHeader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <FirstPage/>
+    <SecondHeader/>
   </React.StrictMode>
 );
 
